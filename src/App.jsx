7e39cd092565c725741e03fec43b9d8a46ace300
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-06-14 17:49:41 +0600

    delete div eltment with fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import Header from "./components/Header"
 import TeachingSection from "./components/TeachingSection"
 import ButtonSection from "./components/ButtonSection"
+// import { Fragment } from "react"
 
 function App() {
  
   return (
-    <div>
+    <>
       <Header />
       <main>
        <TeachingSection/>
        <ButtonSection/>
       </main>
-    </div>
+    </>
   )
 }
 
