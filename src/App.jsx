25e6038cd46d699f87532a314b453e371fd3efff
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-06-18 15:34:49 +0600

    localize header styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header"
+import Header from "./components/Header/Header"
 import TeachingSection from "./components/TeachingSection"
 import ButtonSection from "./components/ButtonSection"
 import IntroSection from "./components/IntroSection"
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,18 @@
 import logo from '/logo-name.svg'
 import { useState } from 'react';
-import './Header.css'
+import { styled } from 'styled-components'
+// import './Header.css'
+
+const HeaderContainer = styled.header`
+    height: 50px;
+    display: flex;
+    padding: 0 2rem;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #ccc;
+    background: #fafafa;
+`
+
 
 export default function Header() {
   const[now, setNow] = useState(new Date())
@@ -8,11 +20,11 @@ export default function Header() {
   setInterval(()=>setNow(new Date()),1000)
    
     return (
-      <header>
+      <HeaderContainer>
         <img src={logo} alt={'Result'} />
        
       
         <span>Время сейчас: { now.toLocaleTimeString() }</span>
-      </header>
+      </HeaderContainer>
     )
   }
