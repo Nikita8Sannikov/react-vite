276bbb11da2b996634ddd3d01a78b0240bd0693b
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-06-18 10:18:48 +0600

    add intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import TeachingSection from "./components/TeachingSection"
 import ButtonSection from "./components/ButtonSection"
+import IntroSection from "./components/IntroSection"
 // import { Fragment } from "react"
 
 function App() {
@@ -9,8 +10,9 @@ function App() {
     <>
       <Header />
       <main>
-       <TeachingSection/>
-       <ButtonSection/>
+      <IntroSection/>
+      <TeachingSection/>
+      <ButtonSection/>
       </main>
     </>
   )
---
+++ src/components/IntroSection.jsx
@@ -0,0 +1,8 @@
+export default function IntroSection() {
+    return(
+        <section>
+            <h1 className="centered">Lorem, ipsum.</h1>
+            <h3 className="centered">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, nemo? Ipsum laboriosam corrupti magnam voluptatem aspernatur laborum placeat animi!</h3>
+        </section>
+    )
+}
