ba4eb9c16a0352e6633158fd78f5cc10f65146e6
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-06-18 16:31:25 +0600

    add form

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,16 @@
 import classes from './Button.module.css'
 
-console.log(classes);
+// console.log(classes);
 
-export default function Button({children, onClick, isActive}) {
+export default function Button({children, onClick, isActive, ...props}) {
     // let classes = 'button'
 
     // if(isActive) classes += ' active'
 
     return( 
-    <button className={isActive ? `${classes.button} ${classes.active}`: classes.button} onClick={onClick}>
+    <button
+    {...props}
+    className={isActive ? `${classes.button} ${classes.active}`: classes.button} onClick={onClick}>
     {/* <button className={isActive ? 'button active': 'button'} onClick={onClick}> */}
     {/*  <button className={classes} onClick={onClick}> */}
         {children}
