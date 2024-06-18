2c8414185a4de544fcba83cea5d42af9b4054bdd
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-06-18 10:33:06 +0600

    add jsx reference

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -1,8 +1,17 @@
+// import React from "react"
+
 export default function IntroSection() {
     return(
         <section>
             <h1 className="centered">Lorem, ipsum.</h1>
-            <h3 className="centered">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, nemo? Ipsum laboriosam corrupti magnam voluptatem aspernatur laborum placeat animi!</h3>
+            <h3 className="centered" style={{color: '#666'}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, nemo? Ipsum laboriosam corrupti magnam voluptatem aspernatur laborum placeat animi!</h3>
         </section>
     )
 }
+// export default function IntroSection() {
+//     return React.createElement('section', null, [
+//         React.createElement('h1', {className: 'centered', key: 1 }, 'Lorem ipsum'), 
+//         React.createElement('h3', {className: 'centered', style:{color: '#666'}, key: 2 }, 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, nemo? Ipsum laboriosam corrupti magnam voluptatem aspernatur laborum placeat animi!')])
+       
+    
+// }
